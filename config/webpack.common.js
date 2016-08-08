6210f04f58bc59c26b276632395b32e7754b57f5
Anthanh Pham Trinh <antai.pt@gmail.com>
2016-08-08 11:24:56 +0200

    metadata updated

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -19,7 +19,7 @@ const ProvidePlugin = require('webpack/lib/ProvidePlugin');
  * Webpack Constants
  */
 const METADATA = {
-  title: 'Angular2 Webpack Starter by @gdi2290 from @AngularClass',
+  title: 'swellrt-pad',
   baseUrl: '/',
   isDevServer: helpers.isWebpackDevServer()
 };
