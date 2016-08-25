1b3865a34f3304cf5282ff2682734ec1fdf17909
Alberto J. Rubio <yebenes9@gmail.com>
2016-08-25 18:55:37 +0200

    Cleanup landing screen

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,19 +1,12 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { UserPanelComponent } from '../user-panel';
-import { SwellRTService } from '../services';
+import {Component} from "@angular/core";
+import {Router} from "@angular/router";
+import {UserPanelComponent} from "../user-panel";
+import {SwellRTService} from "../services";
 
 @Component({
-    selector: 'app-landing',
-    template: `
+  selector: 'app-landing',
+  template: `
     <div class="row">
-
-      <!-- <div class="col-md-3">
-        <app-user-panel #userPanel></app-user-panel>
-      </div>
-
-      <div class="col-md-6 col-md-offset-1"> -->
-
         <div class="alert alert-dismissible alert-danger" *ngIf="wasError">
           <button type="button" class="close" data-dismiss="alert" (click)="wasError = false">×</button>
           <strong>{{msgError}}</strong>
@@ -88,33 +81,12 @@ import { SwellRTService } from '../services';
             </div>  
           </div>
         </div>
-
-        <!--<div class="panel panel-default">
-          <form class="panel-body" (ngSubmit)="openDocument(documentId);" #documentIdForm="ngForm">
-            <a (click)="documentIdInput.focus()"><h3>Open a Document</h3></a>
-            <p>
-              Do you have a shared document ID? Use it to open the document again...
-            </p>
-
-            <div class="form-group label-floating">
-              <label class="control-label" for="documentIdInput">Document ID here</label>
-              <input [(ngModel)]="documentId" name="documentId" #documentIdInput required class="form-control" id="documentIdInput">
-            </div>
-
-            <button class="btn btn-primary pull-right" [disabled]="!documentIdForm.form.valid">Open</button>
-          </form>
-        </div>-->
-
-      <!--</div>-->
-
     </div>
-    <app-login></app-login>
     <app-footer></app-footer>
     `,
 
-    directives: [UserPanelComponent]
-  })
-
+  directives: [UserPanelComponent]
+})
 
 
 export class LandingComponent {
@@ -122,12 +94,7 @@ export class LandingComponent {
   wasError: boolean = false;
   msgError: string;
 
-  constructor(private swellrt: SwellRTService, private router: Router) {
-   }
-
-  createDocument() {
-    let randomId = btoa(Math.random().toString(36)).replace(/=/g, '');
-    this.openDocument(randomId);
+  constructor(private router: Router) {
   }
 
   openDocument(_id: string) {
@@ -137,7 +104,7 @@ export class LandingComponent {
       return;
     }
     this.wasError = false;
-    let link = ['edit', _id ];
+    let link = ['edit', _id];
     this.router.navigate(link);
   }
 
