f58c49a22117b902475fa82fb64a183c99b7bd15
Alberto J. Rubio <yebenes9@gmail.com>
2016-08-31 18:08:28 +0200

    Lateral menu finished

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,17 @@
-import {Component} from "@angular/core";
+import {Component, ElementRef, ViewChild} from "@angular/core";
 
 @Component({
   selector: 'app-header',
   template: `
         <header>
           <div class="row">
+            <div #lateralMenu class="lateral-menu">
+              <div>
+                <h4 class="lateral-menu-title text-center">MY DOCUMENTS</h4>
+                <a href="javascript:void(0)" class="close-button" (click)="closeLateralMenu()">&times;</a>
+              </div>
+              <hr>
+            </div>
             <nav class="navbar navbar-default">
               <div class="container-fluid">
                 <div class="navbar-header">
@@ -12,7 +19,9 @@ import {Component} from "@angular/core";
                 </div>
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                   <ul class="nav navbar-nav navbar-right">
-                    <app-user-panel></app-user-panel>         
+                    <li><app-user-panel></app-user-panel></li>
+                    <li><span class="glyphicon glyphicon-menu-hamburger lateral-menu-button navbar-brand" 
+                              aria-hidden="true" (click)="openLateralMenu()"></span></li>
                   </ul>
                 </div>
               </div>
@@ -24,7 +33,19 @@ import {Component} from "@angular/core";
 
 export class HeaderComponent {
 
-  constructor() {
+  @ViewChild('lateralMenu') lateralMenu: ElementRef;
+
+  constructor() {}
+
+  openLateralMenu() {
+    this.changeLateralMenuWidth("33%");
   }
 
+  closeLateralMenu() {
+    this.changeLateralMenuWidth("0");
+  }
+
+  changeLateralMenuWidth(width: string) {
+    this.lateralMenu.nativeElement.style.width = width;
+  }
 }
