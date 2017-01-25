d333a57fe7b3414ccaf37a9873a91d70c55a1af1
alexseik <alexseik@hotmail.com>
2017-01-25 11:52:03 +0100

    Refactor editor header to be responsive

--- src/app/editor/components/header/editor-header.component.ts
+++ src/app/editor/components/header/editor-header.component.ts
@@ -6,5 +6,15 @@ import { Component, OnInit, OnDestroy } from "@angular/core";
 })
 
 export class EditorHeaderComponent {
-
+  participants = [{
+    name: 'pepe'
+  },{
+    name: 'emilio'
+  },{
+    name: 'rodrigo'
+  },{
+    name: 'fernando'
+  },{
+    name: 'casamayor'
+  }];
 }
