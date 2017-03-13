bdefbd554b65cabc4a734c1eb2ccc311d93ecbff
Pablo Ojanguren <pablojan@gmail.com>
2017-03-13 12:16:22 +0100

    Fix #50 force document names to lowercase

--- src/app/site/components/landing/landing.component.ts
+++ src/app/site/components/landing/landing.component.ts
@@ -12,7 +12,18 @@ export class LandingComponent {
 
   openDocument(_id: string) {
     if (_id) {
-      _id = _id.split(" ").join("-").substr(0, 64);
+      // [$&+,:;=?@#|'<>.-^*()%!]" -> regexp special chars
+      // remove all special chars of URIs and regexp
+      /*
+        Test:
+        var s = "A[B$C&D+E,F:G;H=I?J@K#L|M'N<O>P.Q-R^S*U(V)W%X!Y]Z\"A";
+        var pattern = /[\:/\?\#\[\]\@\!\$\&\'\(\)\*\+\,\;\=\"\|\<\>\-\^\%\.]/g
+        s.replace(pattern, s) == "ABCDEFGHIJKLMNOPQRSUVWXYZA"
+      */
+
+      let pattern = /[\:/\?\#\[\]\@\!\$\&\'\(\)\*\+\,\;\=\"\|\<\>\-\^\%\.]/g
+      _id = _id.replace(pattern, "");
+      _id = _id.split(" ").join("-").substr(0, 64).toLowerCase();
       let link = ['edit', _id];
       this.router.navigate(link);
     }
